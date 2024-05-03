63466b22cbb78006c868949d1522c23d3185c833
IT21819292 <it21819292@my.sliit.lk>
2024-05-04 01:25:32 +0530

    update schema

---
+++ BACKEND/models/regularroute.js
@@ -0,0 +1,24 @@
+const mongoose =  require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+const regularrouteSchema = new Schema({
+
+    rdayid : {
+        type : String,
+        required: true
+    },
+    rday : {
+        type : String,
+        required: true
+    },
+    rroute : {
+        type : String,
+        required: true
+    }
+
+})
+
+const RegularRoute = mongoose.model("RegularRoute",regularrouteSchema);
+module.exports = RegularRoute;
